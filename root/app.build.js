1831a0f5bd52377151168e634b7aa5aed489d24a
Herbert Vojčík <herby@mailbox.sk>
2014-07-27 15:53:41 +0200

    comments are comments in app.build.js

--- root/app.build.js
+++ root/app.build.js
@@ -1,9 +1,11 @@
 ({
     mainConfigFile: "config.js",
     paths: {
-        __comment1: "paths for jquery-ui and jquery must be set manually",
-        __comment2: "because r.js is unable to process array path mapping",
-        __comment3: "how to finally solve this is open issue",
+        // paths for jquery-ui and jquery must be set manually
+        // because r.js is unable to process array path mapping
+        // how to finally solve this is open issue
+        // if these do not match your config, try the alternative one
+        // find the alternatives inside your config.js paths: section
         "jquery-ui": "bower_components/jquery-ui/jquery-ui.min",
         "jquery": "bower_components/jquery/jquery.min"
     },
