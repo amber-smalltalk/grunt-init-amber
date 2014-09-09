10590f98f5ba337b318fbbf1869b86f47357df94
Herbert Vojčík <herby@mailbox.sk>
2014-09-09 23:06:47 +0200

    app.build.js does not need explicit paths mapping...
    ... for jquery[-ui], provided it uses newest amber-cli,
    which uses amber-dev ~0.2.0, which find which of the files
    actually exists in amber config and use that one directly in config.js.

--- root/app.build.js
+++ root/app.build.js
@@ -1,14 +1,5 @@
 ({
     mainConfigFile: "config.js",
-    paths: {
-        // paths for jquery-ui and jquery must be set manually
-        // because r.js is unable to process array path mapping
-        // how to finally solve this is open issue
-        // if these do not match your config, try the alternative one
-        // find the alternatives inside your config.js paths: section
-        "jquery-ui": "bower_components/jquery-ui/jquery-ui.min",
-        "jquery": "bower_components/jquery/jquery.min"
-    },
     include: ['{%= namespace %}/{%= name %}', 'amber/deploy', 'amber/requirejs/require.min'],
     out: "all-in-1.js"
 })
