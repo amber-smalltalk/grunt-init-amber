1a3575a2d4ed52af35d70e8988a3c9b743455924
Herbert Vojčík <herby@mailbox.sk>
2014-09-12 22:08:00 +0200

    {devel,deploy}.js: delimiter comments

--- root/deploy.js
+++ root/deploy.js
@@ -1,6 +1,8 @@
 define([
     'amber/deploy',
+    // --- packages used only during development begin here ---
     '{%= namespace %}/{%= name %}'
+    // --- packages used only during development end here ---
 ], function (smalltalk) {
     return smalltalk;
 });
--- root/devel.js
+++ root/devel.js
@@ -1,7 +1,9 @@
 define([
     'amber/devel',
     'deploy',
+    // --- packages to be deployed begin here ---
     '{%= namespace %}/{%= name %}-Tests'
+    // --- packages to be deployed end here ---
 ], function (smalltalk) {
     return smalltalk;
 });
