d87a05ef18578fafbc42cf9f6d48367e1edb6061
Herbert Vojčík <herby@mailbox.sk>
2014-10-01 19:29:53 +0200

    pragmas to exclude marked parts in deploy

--- root/deploy.build.js
+++ root/deploy.build.js
@@ -3,6 +3,10 @@
     onBuildWrite: function (moduleName, path, contents) {
         return moduleName === "config" ? contents + "\nrequire.config({map:{'*':{app:'deploy'}}});" : contents;
     },
+    pragmas: {
+        excludeIdeData: true,
+        excludeDebugContexts: true
+    },
     include: ['config', 'node_modules/requirejs/require', 'deploy'],
     out: "the.js"
 })
