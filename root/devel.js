30c3218797b883d9336492a9571ac3c0dc4abbbb
Herbert Vojčík <herby@mailbox.sk>
2015-01-22 00:13:13 +0100

    Load Benchfib and Examples from attic as well
    while in development mode

--- root/devel.js
+++ root/devel.js
@@ -3,6 +3,8 @@ define([
     './deploy',
     // --- packages used only during development begin here ---
     '{%= namespace %}/{%= name %}-Tests',
+    'amber-attic/Benchfib',
+    'amber-attic/Examples',
     'amber-attic/IDE'
     // --- packages used only during development end here ---
 ], function (amber) {
